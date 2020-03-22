437c58b438bf821f974cfe76e033620aa4d944af
Alex <Alex@DESKTOP-CEQG6CC>
2020-03-22 14:54:41 +0200

    dcdc

--- Car/Basic_source.cpp
+++ Car/Basic_source.cpp
@@ -33,12 +33,19 @@ class Car
 			}
 		}
 
-		double give_fuel(double amount)
+		double give_fuel(double amount, unsigned int speed)
 		{
+			if (1 < speed && speed < 60) amount = 0.002;
+			if (61 < speed && speed < 100) amount = 0.014;
+			if (101 < speed && speed < 140) amount = 0.002;
+			if (141 < speed && speed < 200) amount = 0.0025;
+			if (201 < speed && speed < 250) amount = 0.003;
 			fuel -= amount;
 			if (fuel <= 0)fuel = 0;
 			return fuel;
 		}
+
+		
 		void info() const
 		{
 			std::cout << "Tank volume:\t" << volume << " liters. \n";
@@ -93,14 +100,28 @@ class Car
 	{
 		std::thread panel_thread;
 		std::thread engine_idle_thread;
+
+		std::thread main_thread; //поток управление потоками
+		std::thread acceliration_thread; // ускорение
+		std::thread free_wheeling_thread; // машина в движении
+
+
 	} control;
 
+	
 	bool driver_inside = false;
+
+	bool gas_pedal;
+	bool break_pedal;
+	unsigned int speed;
+	unsigned int max_speed;
+
+	
 
 public:
 	const Engine& get_engine() const {return this->engine; }
 	const Tank& get_tank() const {return this->tank; }
-
+	const unsigned int& get_speed() const { return speed; }
 	void fill(double fuel) { tank.fill(fuel); }
 
 	void get_in()
@@ -112,6 +133,7 @@ public:
 	{	
 		driver_inside = false;
 		if (control.panel_thread.joinable()) control.panel_thread.join();	
+		system("cls");
 	}
 
 	void panel() 
@@ -120,6 +142,7 @@ public:
 		{
 			system("cls");
 			std::cout << "Engine is "; /* << (engine.Started() ? "started" : "stoped") << ".\n";*/
+			
 			if  (engine.Started())
 			{
 				Text_color(10);
@@ -134,7 +157,8 @@ public:
 
 			}
 			std::cout << "Fuel level: " << tank.get_fuel() << std::endl;
-			
+			std::cout << "Speed:      " << speed << " km/h" << std::endl;
+
 			if (tank.get_fuel() < 5)
 			{				
 			 	Text_color(12);
@@ -167,7 +191,7 @@ public:
 
 	void engine_idle()
 	{
-		while (engine.Started() && tank.give_fuel(engine.get_consumption_per_second()))
+		while (engine.Started() && tank.give_fuel(engine.get_consumption_per_second(),get_speed()))
 		{
 			std::this_thread::sleep_for(1s);			
 		}
@@ -184,55 +208,96 @@ public:
 		W_line();
 	}
 
-	Car(double engine, unsigned int tank) :engine(engine), tank(tank)
+	Car(double engine, unsigned int tank, unsigned max_speed = 250) :engine(engine), tank(tank)
 	{
+		driver_inside = false;
+		gas_pedal = false;
+		break_pedal = false;
+		speed = 0;
+		this->max_speed = max_speed<= 350 ? max_speed : max_speed;
 		std::cout << "You car is ready to go:-)\n";
+
+		control.main_thread = std::thread(&Car::ControlCar, this);
 	}
 
 	~Car()
 	{
+		if (control.main_thread.joinable()) control.main_thread.join();
 		std::cout << "Don't leave me alone.\n";
 	}
+
+	void ControlCar()
+	{
+		try
+		{
+			char key;
+			do
+			{
+				key = _getch();
+				switch (key)
+				{
+				case 13:
+					if (driver_inside)this->get_out();
+					else this->get_in();
+					break;
+				case 'i':case 'I': //Ignitton
+
+					if (!this->get_engine().Started())this->start();
+					else this->stop();
+					break;
+				case 'f':
+					double amount;
+					std::cout << "How match?: ";
+					std::cin >> amount;
+					this->fill(amount);
+					break;
+				case 'w':case 'W':
+					std::this_thread::sleep_for(0.6s);
+					if (engine.Started())
+						if (speed < max_speed)
+						{
+							speed += 10;							
+						}
+						else speed = max_speed;
+					
+					break;
+				case 's':case 'S':
+					if (speed > 20) speed -= 20;
+					else speed = 0;
+					break;
+				case 27:
+					this->stop();
+					this->get_out();
+					break;
+				}
+				std::this_thread::sleep_for(0.1s);
+				if (!control.free_wheeling_thread.joinable() && speed >0) control.free_wheeling_thread = std::thread(&Car::free_wheeling, this);
+				else if (control.free_wheeling_thread.joinable()&& speed == 0) control.free_wheeling_thread.join();
+			} while (key != 27);
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	
+	void free_wheeling()
+	{
+		while (speed > 0)
+		{
+			speed--;
+			std::this_thread::sleep_for(1s);
+		}
+	}
 };
+
+
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 
 	Car car(8,40);	
-	
-
 	std::cout << "Press Enter to get in";
-	std::cin.get();
-	car.get_in();
-
-	try
-	{
-		char key;
-
-		do
-		{
-			key = _getch();
-			switch (key)
-			{
-			case 13:
-				if (!car.get_engine().Started())car.start();
-				else car.stop();
-				break;
-			case 'f':
-				double amount;
-				std::cout << "How match?: ";
-				std::cin >> amount;
-				car.fill(amount);
-				break;
-			case 27:
-				car.stop();
-				car.get_out();
-			}
-		} while (key != 27);
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	
 }
